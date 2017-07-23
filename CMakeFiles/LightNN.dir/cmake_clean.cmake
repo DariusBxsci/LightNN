d409f714363d829b7efb150999e558e26f956a20
Axtya Darius <barbanod@bxscience.edu>
2017-07-22 21:08:37 -0400

    new

---
+++ CMakeFiles/LightNN.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LightNN.dir/include/network.o"
+  "libLightNN.pdb"
+  "libLightNN.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LightNN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
